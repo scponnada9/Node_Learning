5f180af894be017100ab1f2986a3ad371ac0c89a
Srinivasa Chaitanya <scponnada9@gmail.com>
2025-02-10 22:43:44 +0000

    db changes3

--- nodejs_course/db.js
+++ nodejs_course/db.js
@@ -1,35 +1,35 @@
 const mongoose = require('mongoose');
 
-//const mongoURL = 'mongodb://localhost:27017/hotels';
-const DB_URL = process.env.DB_URL;
-mongoose.connect(DB_URL,{
+const mongoURL = 'mongodb://localhost:27017/hotels';
+//const DB_URL = process.env.DB_URL;
+mongoose.connect(mongoURL,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 })
 //Get the default conenction
-//const db = mongoose.connection;
+const db = mongoose.connection;
 
-const db = async () => {
-    try {
-      const conn = await mongoose.connect(process.env.DB_URL);
-      console.log("CONNECTED TO DATABASE SUCCESSFULLY");
-    } 
-    catch (error) {
-        console.error('COULD NOT CONNECT TO DATABASE:', error.message);
-    }
-  }
+// const db = async () => {
+//     try {
+//       const conn = await mongoose.connect(process.env.DB_URL);
+//       console.log("CONNECTED TO DATABASE SUCCESSFULLY");
+//     } 
+//     catch (error) {
+//         console.error('COULD NOT CONNECT TO DATABASE:', error.message);
+//     }
+//   }
 
 
-// db.on('connected',() => {
-//     console.log('Connected to MongoDB');
-// });
+ db.on('connected',() => {
+    console.log('Connected to MongoDB');
+ });
 
-// db.on('error',() => {
-//     console.log('Error while connecting to MongoDB');
-// });
+ db.on('error',() => {
+    console.log('Error while connecting to MongoDB');
+ });
 
-// db.on('disconnected',() => {
-//     console.log('Disconnected to MongoDB');
-// });
+ db.on('disconnected',() => {
+     console.log('Disconnected to MongoDB');
+ });
 
 module.exports = db;
