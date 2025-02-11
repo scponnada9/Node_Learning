7a6fd9ff8cc75dfdd18eeca47069100fc4f3cfc3
Srinivasa Chaitanya <scponnada9@gmail.com>
2025-02-11 12:37:16 +0000

    DB PORT FIX

--- nodejs_course/server.js
+++ nodejs_course/server.js
@@ -8,6 +8,15 @@ const db = require('./db');
 const bodyParser = require('body-parser');
 app.use( bodyParser.json());
 
+const passport = require('passport');
+const passport_local = require('passport-local');
+
+const logRequest = (req, res, next) => {
+    console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.OriginalUrl}`);
+    next(); //Move yto next phase
+}
+
+app.use(logRequest);
 const personRoutes = require('./Routes/personRoutes');
 const menuRoutes = require('./Routes/menuRoutes');
 
